87181b06700bc8ae4cc4bf29447205bb6cf308bb
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-10-26 21:24:57 +0700

    adding some airbnb rule like

--- index.ts
+++ index.ts
@@ -1,11 +1,36 @@
 // semi colon true: OK
-const hello = () => {
+const hello = (): void => {
   console.log('helload');
 };
 
 hello();
 
-// trailing comma
+// trailing comma: OK
 const obj = {
   one: 'one',
 };
+
+console.log(obj);
+
+// block spacing: OK
+const somename ='ad';
+console.log(somename);
+
+// brace style:
+function example(): void {
+
+}
+example();
+
+// comma spacing
+const obj2 = { 'foo': 'bar', 'baz': 'qur' };
+
+console.log(obj2);
+
+// func-call-spacing
+const exampleFunction = (): void => {};
+exampleFunction();
+
+// key-spacing
+const obj3 = { 'foo': 42 };
+console.log(obj3);
